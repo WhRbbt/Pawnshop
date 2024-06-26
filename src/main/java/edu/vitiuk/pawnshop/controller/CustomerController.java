ef6a8bd96054863b66f15fddb420645300df219d
Oleksandr Vitiuk <96valex@gmail.com>
2024-06-26 16:00:21 +0300

    Implemented success message after deleting

--- src/main/java/edu/vitiuk/pawnshop/controller/CustomerController.java
+++ src/main/java/edu/vitiuk/pawnshop/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/customers")
@@ -50,8 +51,9 @@ public class CustomerController {
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteCustomer(@PathVariable Long id) {
+    public String deleteCustomer(@PathVariable Long id, RedirectAttributes redirectAttributes) {
         customerService.deleteById(id);
+        redirectAttributes.addFlashAttribute("message", "Customer successfully deleted.");
         return "redirect:/customers";
     }
 }
