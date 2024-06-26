d327b8579406d492b54a152957cb1865d7928598
Oleksandr Vitiuk <96valex@gmail.com>
2024-06-26 11:45:58 +0300

    Created Spring Boot Project

---
+++ src/test/java/edu/vitiuk/pawnshop/PawnshopApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vitiuk.pawnshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawnshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
